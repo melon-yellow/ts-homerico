f24aa5ad350187fdf725b2883d637e877831f65f
anthony-freitas <78743473+anthony-freitas@users.noreply.github.com>
2021-11-03 15:36:08 -0300

    dsfwucu9wes9f9iisedfw

--- src/utils/funcoes.ts
+++ src/utils/funcoes.ts
@@ -8,15 +8,9 @@
 export function toHex(str: string) {
   // Set Variables
   let result: string = ''
-  let bytes: number[] = []
   // Iterate over String Char
-  Array(str.length).forEach((_v, i) => {
-    const code = str.charCodeAt(i)
-    bytes = bytes.concat([code & 0xff, code / 256 >>> 0])
-  })
-  // Iterate over Bytes
-  bytes.forEach(v => {
-    const char = v.toString(16)
+  str.split('').forEach(v => {
+    const char = v.charCodeAt(0).toString(16)
     result += `${char.length < 2 ? '0' : ''}${char}`
   })
   // Return Hex String
